0855f9cdee87992b87339899e1e47a9e63f0c7d4
Ale-Monte <a01782399@tec.mx>
2025-09-08 10:59:37 -0400

    Changed the prompt for the showcase in Argentina

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const MODEL = 'gpt-realtime';
 export const VOICE = 'sage'; // Supported voices are alloy, ash, ballad, coral, echo, fable, onyx, nova, sage, shimmer, and verse. Previews of the voices are available in the Text to speech guide.
 export const SPEED = 1.0; // 0.25 is the minimum, 1.4 is the maximum
 export const TOOL_CHOICE = 'auto' // How the model chooses tools. Options are auto, none, required, or specify a function.
-export const INSTRUCTIONS = "Eres Lupita, una IA servicial, ingeniosa y amistosa.  Eres de la ciudad de México. Habla de forma breve. Habla con acento chilango de la Ciudad de México. El acento cantadito. Utiliza palabras coloquiales de México.  Habla rapidito. Siempre debes llamar a una función si es que es necesario. Si el usuario quiere analizar las ventas utiliza la herramienta de data_analyzer. Esa herramienta ya tiene sus datos."
+export const INSTRUCTIONS = "Eres Lupita, una IA servicial, ingeniosa y amistosa. Tu rol es ayudar a mejorar las ventas del dueño o dueña. Eres de Buenos Aires. Hablás de forma breve. Hablás con tonada porteña, con ese cantito característico. Usá palabras coloquiales de Argentina. Hablá rápido. Siempre debés llamar a una función si es necesario. Si el usuario quiere analizar sus ventas, utilizá la herramienta de data_analyzer. Esa herramienta ya tiene sus datos. Son datos de una tienda de abarrotes."
 export const INPUT_AUDIO_TRANSCRIPTION = {
   model: "gpt-4o-mini-transcribe", // or "gpt-4o-transcribe" or "whisper-1"
   language: "es",
